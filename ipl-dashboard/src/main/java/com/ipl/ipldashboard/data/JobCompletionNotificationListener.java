415f24c962c78e1100468e86873cd7d8e7f640f5
Panchajanya <panchajanyamysarla@gmail.com>
2021-04-24 17:33:31 -0500

    react tictactoe and ipl dashboard

--- ipl-dashboard/src/main/java/com/ipl/ipldashboard/data/JobCompletionNotificationListener.java
+++ ipl-dashboard/src/main/java/com/ipl/ipldashboard/data/JobCompletionNotificationListener.java
@@ -1,6 +1,7 @@
 package com.ipl.ipldashboard.data;
 
 import com.ipl.ipldashboard.model.Match;
+import com.ipl.ipldashboard.model.Team;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.BatchStatus;
@@ -10,26 +11,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import javax.persistence.EntityManager;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Component
 public class JobCompletionNotificationListener extends JobExecutionListenerSupport {
 
     private static final Logger log = LoggerFactory.getLogger(JobCompletionNotificationListener.class);
 
-    private final JdbcTemplate jdbcTemplate;
+    private final EntityManager entityManager;
 
     @Autowired
-    public JobCompletionNotificationListener(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public JobCompletionNotificationListener(EntityManager entityManager) {
+        this.entityManager = entityManager;
     }
 
     @Override
     public void afterJob(JobExecution jobExecution) {
         if(jobExecution.getStatus() == BatchStatus.COMPLETED) {
             log.info("!!! JOB FINISHED! Time to verify the results");
+            Map<String, Team> teamData = new HashMap<>();
 
-            jdbcTemplate.query("SELECT team1, team2, date FROM match",
-                    (rs, row) ->  "Team1"+ rs.getString(1)+ "Team2" +rs.getString(2) + "Date" +rs.getString(3)
-            ).forEach(match -> log.info("Found <" + match + "> in the database."));
+
+
+            entityManager.createQuery("select m.team1, count(*) from Match m group by m.team1")
+            .getResultList()
+                    .stream()
+                    .map(e -> new Team((String) e[0], (long) e[1]))
+                    .forEach(team -> teamData.put(((Team) team).getTeamName(),(Team) team));
+
+//
+//            jdbcTemplate.query("SELECT team1, team2, date FROM match",
+//                    (rs, row) ->  "Team1"+ rs.getString(1)+ "Team2" +rs.getString(2) + "Date" +rs.getString(3)
+//            ).forEach(match -> log.info("Found <" + match + "> in the database."));
         }
     }
 }
---
+++ ipl-dashboard/src/main/java/com/ipl/ipldashboard/model/Team.java
@@ -0,0 +1,26 @@
+package com.ipl.ipldashboard.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Team {
+
+    @Id
+    private long Id;
+    private String teamName;
+    private long totalMatches;
+    private long totalWins;
+
+    public Team(String teamName, long totalMatches) {
+        this.teamName = teamName;
+        this.totalMatches = totalMatches;
+    }
+}
